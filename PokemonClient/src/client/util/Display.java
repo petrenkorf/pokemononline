046696cbd4785b5a76aa47b470a2c49d2da54021
bruno.weig <bruno.weig@gmail.com>
2014-05-22 23:22:55 -0300

    Arquivo client.util.Display.java

---
+++ PokemonClient/src/client/util/Display.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package client.util;
+
+import java.awt.DisplayMode;
+import java.awt.GraphicsEnvironment;
+
+/**
+ *
+ * @author bruno.weig
+ */
+public class Display {
+    GraphicsEnvironment e = null;
+    static Display instance = null;
+    
+    DisplayMode currentDisplayMode = null;
+    
+    private Display() {
+        e = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        
+        currentDisplayMode = e.getDefaultScreenDevice().getDisplayMode();
+    }
+    
+    static public Display getInstance() {
+        if ( instance == null ) {
+            instance = new Display();
+        }
+        
+        return instance;
+    }
+    
+    public DisplayMode[] getResolutions() {
+        return e.getDefaultScreenDevice().getDisplayModes();
+    }
+    
+    public void setCurrentDisplay(DisplayMode mode) {
+        DisplayMode[] res = getResolutions();
+        
+        boolean found = false;
+        
+        for (DisplayMode r : res) {
+            if (mode.getWidth() == r.getWidth() && mode.getHeight() == r.getHeight()) {
+                found = true;
+                break;
+            }
+        }
+        
+        if ( found ) {
+            currentDisplayMode = mode;
+            System.out.println("Found");
+        } else {
+            currentDisplayMode = e.getDefaultScreenDevice().getDisplayMode();
+            System.out.println("Not Found");
+        }
+    }
+    
+    public DisplayMode getCurrentDisplayMode() {
+        return currentDisplayMode;
+    }
+}
